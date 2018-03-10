afb465bb4fdea105c2b654243bfb017fa5a55f81
Roman <detininroman@gmail.com>
2018-03-10 13:45:37 +0300

    add structure and dividing

--- Integral/main.c
+++ Integral/main.c
@@ -1,22 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+typedef struct thread_data {
+    double from;
+    double to;
+    double dx;
+    double pieces_num;
+    double sum;
+} thread_data;
 
 double f(double x)
 {
     return x*x;
 }
 
-double integrate(double a, double b)
+void* integrate(void* arg)
 {
-    double result = 0, N = 1e+9, dx = (b-a)/N;
+    thread_data* data = (thread_data*) arg;
     
-    for (double x = a; x < b; x += dx)
-        result += f(x) * dx;
+    for (double x = data->from; x < data->to; x += data->dx)
+        data->sum += f(x) * data->dx;
     
-    return result;
+    return NULL;
 }
 
-int main(void)
+int main(int argc, char** argv)
 {
-    printf("Result: %lg\n", integrate(0.00, 5.00));
+    if (argc != 2) {
+        fprintf(stderr, "Error: %s\n", strerror(EINVAL));
+        exit(EXIT_FAILURE);
+    }
+
+    double A = 0;
+    double B = 50;
+    double N = 1000000;
+
+    long n_threads = strtoll(argv[1], NULL, 10);
+    double segment_length = (B - A) / n_threads; 
+    double total = 0;
+
+    thread_data* data = calloc(n_threads, sizeof(*data));
+    
+    for (int i = 0; i < n_threads; i++) {
+        data[i].from       = A + segment_length * i;
+        data[i].to         = data[i].from + segment_length;
+        data[i].pieces_num = N / n_threads;
+        data[i].dx         = segment_length / data[i].pieces_num;
+        data[i].sum        = 0;
+             
+        integrate(data + i);
+        total += data[i].sum;
+    }
+    free(data);
+ 
+    printf("Result: %lg\n", total);
     return 0;
 }
