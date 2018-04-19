5deb2cda3088bd75d2bf1b1b28f8ea2c0e955ab9
Roman <detininroman@gmail.com>
2018-04-19 13:01:04 +0300

    divided into threads

--- Integral/main.c
+++ Integral/main.c
@@ -2,26 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <unistd.h>
+#include <pthread.h>
 
-typedef struct thread_data {
+struct thread_data {
     double from;
     double to;
-    double dx;
     double pieces_num;
     double sum;
-} thread_data;
+    pthread_t thread_ptr;
+};
 
 double f(double x)
 {
-    return x*x;
+    return x*x + x/(1+x);
 }
 
 void* integrate(void* arg)
 {
-    thread_data* data = (thread_data*) arg;
+    struct thread_data* data = (struct thread_data*)arg;
     
-    for (double x = data->from; x < data->to; x += data->dx)
-        data->sum += f(x) * data->dx;
+    double dx = (data->to - data->from) / data->pieces_num;
+    for (double x = data->from; x < data->to; x += dx)
+        data->sum += f(x) * dx;
     
     return NULL;
 }
@@ -33,26 +36,30 @@ int main(int argc, char** argv)
         exit(EXIT_FAILURE);
     }
 
-    double A = 0;
-    double B = 50;
-    double N = 1000000;
+    double A = 0;    // from
+    double B = 50;   // to
+    double N = 1e+9; // 10^9
 
-    long n_threads = strtoll(argv[1], NULL, 10);
+    long n_threads = strtol(argv[1], NULL, 10);
     double segment_length = (B - A) / n_threads; 
     double total = 0;
 
-    thread_data* data = calloc(n_threads, sizeof(*data));
-    
+    struct thread_data* data = calloc(n_threads, sizeof(*data));
+
     for (int i = 0; i < n_threads; i++) {
         data[i].from       = A + segment_length * i;
         data[i].to         = data[i].from + segment_length;
         data[i].pieces_num = N / n_threads;
-        data[i].dx         = segment_length / data[i].pieces_num;
-        data[i].sum        = 0;
-             
-        integrate(data + i);
+    }
+
+    for (int i = 0; i < n_threads; i++)
+        pthread_create(&data[i].thread_ptr, NULL, integrate, data + i);
+    
+    for (int i = 0; i < n_threads; i++) {
+        pthread_join(data[i].thread_ptr, NULL);
         total += data[i].sum;
     }
+
     free(data);
  
     printf("Result: %lg\n", total);
